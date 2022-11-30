a0a3f1d703a59553822cc7787716069b2fa67f61
Felipe Salomao <lipesalomaoclaf@gmail.com>
2022-11-30 19:33:34 -0300

    fix

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,8 +1,7 @@
 import Prismic from "@prismicio/client";
 
 export function getPrismicClient(req?: unknown) {
-  const client = Prismic.client(`${process.env.PRISMIC_ENDPOINT}`, {
-    req,
+  const client = Prismic.createClient(`${process.env.PRISMIC_ENDPOINT}`, {
     accessToken: `${process.env.PRISMIC_ACCESS_TOKEN}`,
   });
 
